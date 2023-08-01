c51afa3e0941f3b3f0df995bd86d57399c210413
minh0811 <vokhaiminh0811@gmail.com>
2023-08-01 11:18:50 +0700

    Initial Commit

---
+++ ViewLurker/ViewLurkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewLurkerApp.swift
+//  ViewLurker
+//
+//  Created by Minh Vo on 01/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ViewLurkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
